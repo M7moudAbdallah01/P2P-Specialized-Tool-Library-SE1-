5e72e65f6c4a52c7c4f3f2ae7cbf7d19b76c4add
M7moudAbdallah01 <mahmoudcs50course@gmail.com>
2026-04-03 18:19:02 +0200

    ..

--- conn.php
+++ conn.php
@@ -3,5 +3,7 @@
 
 $conn = new mysqli("localhost", "root", "", "tool_library");
 
-
+if ($conn->connect_error) {
+    die("no connection: " . $conn->connect_error);
+}
 ?>
--- index.php
+++ index.php
@@ -12,9 +12,31 @@ require_once "conn.php";
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title></title>
+        <link rel="stylesheet" href="css/global.css">
+        <link rel="stylesheet" href="css/login.css">
+
+
+        <title>Tool Library</title>
     </head>
     <body>
+
+
+        <form action="login.php" method="POST">
+
+            <label>Email:</label><br>
+            <input type="email" name="email" required><br><br>
+
+            <label>Password:</label><br>
+            <input type="password" name="password" required><br><br>
+
+            <button type="submit">Login</button>
+
+        </form>
+
+        <br>
+
+        <a href="#">Forgot Password?</a><br>
+        <a href="#">Create New Account</a>
         
     </body>
 </html>
