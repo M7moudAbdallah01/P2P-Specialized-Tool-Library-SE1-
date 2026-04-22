1df291e875759d2af4af1b09b2d9c00e0253546d
M7moudAbdallah01 <mahmoudcs50course@gmail.com>
2026-04-22 18:14:48 +0200

    ..

--- Controllers/conn.php
+++ Controllers/conn.php
@@ -1,9 +1,8 @@
 <?php
 
-
 $conn = new mysqli("localhost", "root", "", "tool_library");
 
 if ($conn->connect_error) {
-    die("no connection: " . $conn->connect_error);
+    die("no connection: ");
 }
 ?>
