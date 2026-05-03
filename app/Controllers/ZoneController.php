9e4a69f55eb41999dafcb423ee571df8d268928e
aliamr111 <messicllot@gmail.com>
2026-05-03 02:37:26 -0700

    Update ZoneController.php

--- app/Controllers/ZoneController.php
+++ app/Controllers/ZoneController.php
@@ -1,18 +1,13 @@
-<?php
-// استدعاء الموديل باستخدام المسار النسبي المضمون
-require_once __DIR__ . "/../Models/Zone.php";
+<?phprequire_once __DIR__ . "/../Models/Zone.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['zone'])) {
     $zoneName = $_POST['zone'];
 
-    // نداء دالة الإضافة من الموديل
     Zone::add($zoneName);
 
-    // الرجوع التلقائي لصفحة العرض بعد النجاح
     header("Location: ../Views/Admin/zones.php");
     exit();
 } else {
-    // حماية: لو حد حاول يفتح الملف مباشرة يرجعه لصفحة الزونز
     header("Location: ../Views/Admin/zones.php");
     exit();
 }
