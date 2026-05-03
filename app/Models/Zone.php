460fb16b2a6504a0e9fbd3b6c259b19cde20b4e1
aliamr111 <messicllot@gmail.com>
2026-05-03 03:53:05 -0700

    Update Zone.php

--- app/Models/Zone.php
+++ app/Models/Zone.php
@@ -2,25 +2,23 @@
 require_once __DIR__ . "/../../Core/database.php";
 
 class Zone {
-
     public static function add($name){
         $db = Database::getInstance()->getConnection();
-        
         $safe_name = mysqli_real_escape_string($db, $name);
-
         $query = "INSERT INTO zones (name) VALUES ('$safe_name')";
-        
         return mysqli_query($db, $query);
     }
 
     public static function getAll(){
         $db = Database::getInstance()->getConnection();
-        
         $query = "SELECT * FROM zones";
-        $result = mysqli_query($db, $query);
-        
-        if ($result) {
-            return mysqli_fetch_all($result, MYSQLI_ASSOC);
+        try {
+            $result = mysqli_query($db, $query);
+            if ($result) {
+                return mysqli_fetch_all($result, MYSQLI_ASSOC);
+            }
+        } catch (Exception $e) {
+            return [];
         }
         return [];
     }
