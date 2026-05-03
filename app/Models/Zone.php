ef8466a2e3133ef92c5dfd16a6ddb110d5111719
aliamr111 <messicllot@gmail.com>
2026-05-03 02:20:40 -0700

    Update Zone.php

--- app/Models/Zone.php
+++ app/Models/Zone.php
@@ -1,17 +1,33 @@
 <?php
-require_once "../Core/database.php";
+// استدعاء ملف الربط مع قاعدة البيانات
+require_once __DIR__ . "/../../Core/database.php";
 
 class Zone {
 
+    // دالة لإضافة منطقة جديدة
     public static function add($name){
-        global $conn;
-        $query = "INSERT INTO zones (name) VALUES ('$name')";
-        mysqli_query($conn, $query);
+        $db = Database::getInstance()->getConnection();
+        
+        // تنظيف البيانات لمنع المشاكل الأمنية
+        $safe_name = mysqli_real_escape_string($db, $name);
+
+        $query = "INSERT INTO zones (name) VALUES ('$safe_name')";
+        
+        // تنفيذ الأمر وإرجاع النتيجة (true/false)
+        return mysqli_query($db, $query);
     }
 
+    // دالة لجلب كل المناطق لعرضها في الجدول
     public static function getAll(){
-        global $conn;
-        $result = mysqli_query($conn, "SELECT * FROM zones");
-        return $result;
+        $db = Database::getInstance()->getConnection();
+        
+        $query = "SELECT * FROM zones";
+        $result = mysqli_query($db, $query);
+        
+        if ($result) {
+            // تحويل النتيجة لمصفوفة (Array) عشان الـ View يعرضها بسهولة
+            return mysqli_fetch_all($result, MYSQLI_ASSOC);
+        }
+        return [];
     }
 }
