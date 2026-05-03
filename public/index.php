4dd81400f897efa09ffb7f34bab97fea6487baca
M7moudAbdallah01 <mahmoudcs50course@gmail.com>
2026-05-03 20:36:12 +0300

    .

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+
+// require_once '../Core/App.php';
+// require_once '../Core/Controller.php';
+require_once '../Core/database.php';
+
+define("BASE_URL", '/' . basename(dirname(__DIR__)) . '/public/');
+
+// $app = new App();
