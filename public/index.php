a3a33463479f489e2a08339edaa77041ff18b055
M7moudAbdallah01 <mahmoudcs50course@gmail.com>
2026-05-04 11:54:25 +0300

    index

--- public/index.php
+++ public/index.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 
-// require_once '../Core/App.php';
-// require_once '../Core/Controller.php';
-require_once '../Core/database.php';
+require_once '../Core/App.php';
+require_once '../Core/Controller.php';
+require_once '../Core/Database.php';
 
 define("BASE_URL", '/' . basename(dirname(__DIR__)) . '/public/');
 
-// $app = new App();
+$app = new App();
