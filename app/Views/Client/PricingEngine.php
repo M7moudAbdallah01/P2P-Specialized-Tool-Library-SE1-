e99ee2068c6b759e3412ac79c893abf285dc2dba
IT-PROJECT <yasseinm067@gmail.com>
2026-05-12 01:12:20 +0300

    .

--- app/Views/Client/PricingEngine.php
+++ app/Views/Client/PricingEngine.php
@@ -91,7 +91,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 "error" => "Invalid tool"
             ]);
             exit();
-        }
+        } 
 
         $t = $conn->prepare("
             SELECT name, base_price
